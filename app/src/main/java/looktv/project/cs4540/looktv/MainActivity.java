eca17a1f7c71a52d1f40b130c7f7746720b7f152
PranilDahal <dahalpranil@gmail.com>
2018-11-10 13:09:14 -0800

    Initilize project

---
+++ app/src/main/java/looktv/project/cs4540/looktv/MainActivity.java
@@ -0,0 +1,53 @@
+package looktv.project.cs4540.looktv;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_browse:
+                    showHome();
+                    return true;
+                case R.id.navigation_mytv:
+                    showMytv();
+                    return true;
+                case R.id.navigation_notifications:
+                    showNotifs();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    void showHome(){
+
+    }
+
+    void showMytv(){
+
+    }
+
+    void showNotifs(){
+
+    }
+
+}
